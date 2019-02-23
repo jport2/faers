08d8c36cfacfbfe6ef871821cf608e82b7dd114f
Jake Portanova <jakeportanova@gmail.com>
2019-02-23 12:11:25 -0800

    Updated to get auc with clean ryan

--- sdoct_filter.R
+++ sdoct_filter.R
@@ -1,8 +1,7 @@
-# Checks AUC against 132/399 of the Ryan Set
-
 setwd("/home/jportanova/Downloads")
 library(readr)
 library(dplyr)
+library(stringr)
 sdoct<- read.table("standard_drug_outcome_contingency_table.tsv", sep="\t")
 # sdoct<- read_tsv("standard_drug_outcome_contingency_table.tsv", col_names = FALSE) change 'V' to 'X'
 sdoct <- sdoct %>% rename(drug_concept_id = V1, outcome_concept_id = V2, a = V3, b = V4, c = V5, d = V6)
@@ -30,20 +29,27 @@ sdoct <- left_join(sdoct, concept1, by="standard_concept_id")
 
 sdoct$X2.x <- tolower(sdoct$X2.x)
 sdoct$X2.y <- tolower(sdoct$X2.y)
-sdoct$drugoutcome <- paste0(sdoct$X2.x, sdoct$X2.y)
-ryan <- read.csv("40264_2013_97_MOESM1_ESM.csv") 
-ryan <- ryan %>% rename(drug_concept_id = Drug.Concept.ID)
+sdoct$drugoutcome <- paste0(sdoct$X2.y, sdoct$X2.x)
+ryan <- read.csv("cleanRyan.csv", header=FALSE) 
+# ryan <- ryan %>% rename(drug_concept_id = Drug.Concept.ID)
+# 
+# # to lower
+# ryan$Condition.Name <- tolower(ryan$Condition.Name)
+# ryan$Drug.Concept.Name <- tolower(ryan$Drug.Concept.Name)
+ryan$drugoutcome <- paste0(ryan$V1,ryan$V2)
+sdoct$drugoutcome <- str_replace_all(sdoct$drugoutcome," ","_")
+sdoct$drugoutcome <- as.factor(as.character(sdoct$drugoutcome))
+ryan$drugoutcome <- as.factor(as.character(ryan$drugoutcome))
 
-# to lower
-ryan$Condition.Name <- tolower(ryan$Condition.Name)
-ryan$Drug.Concept.Name <- tolower(ryan$Drug.Concept.Name)
-ryan$drugoutcome <- paste0(ryan$Drug.Concept.Name,ryan$Condition.Name)
 merge <- left_join(sdoct, ryan, by="drugoutcome")
-merge <- merge[complete.cases(merge$Condition.Concept.ID),]
+merge <- merge[complete.cases(merge$V3),]
+dim(merge)
+
+setdiff(ryan$drugoutcome, merge$drugoutcome)
 
 # basicplot <- ggplot(merge, aes(d = Ground.Truth..1.postive.control..0.negative.control., m = prr)) + geom_roc()
 library(pROC)
-plot <-plot.roc(merge$Ground.Truth..1.postive.control..0.negative.control., merge$prr, type="b", pch=21, col="blue", bg="grey")
+plot <-plot.roc(merge$V3, merge$prr, type="b", pch=21, col="blue", bg="grey")
 plot
 plot <-plot.roc(merge$Ground.Truth..1.postive.control..0.negative.control., merge$ror, type="b", pch=21, col="blue", bg="grey")
 plot
